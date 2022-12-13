04fdf3ae66def4b3d9d79564ead1807fe755dc6a
Fellipe Lyra <84457207+FellipeLyra@users.noreply.github.com>
2022-12-13 19:00:32 -0300

    Create Token.java

---
+++ Analisador Léxico/Token.java
@@ -0,0 +1,16 @@
+package Léxico;
+
+public class Token {
+    public TipoToken nome;
+    public String lexema;
+
+    public Token(TipoToken nome, String lexema){
+        super();
+        this.nome = nome;
+        this.lexema =lexema;
+    }
+
+    public String toString(){
+        return "<" + nome + "," + lexema + ">";
+    }
+}
