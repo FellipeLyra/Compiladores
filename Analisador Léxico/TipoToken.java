e4910764866f038844bcea34f39eda73ade62663
Fellipe Lyra <84457207+FellipeLyra@users.noreply.github.com>
2022-12-13 19:00:21 -0300

    Create TipoToken.java

---
+++ Analisador Léxico/TipoToken.java
@@ -0,0 +1,9 @@
+package Léxico;
+
+public enum TipoToken{
+    PCDeclaracoes,PCAlgoritmo,PCInteiro,PCAtribuir,PCImprimir,PCSe,PCEntao,
+    PCEnquanto,PCInicio,PCFim,OprAritMult,OprAritDiv,OprAritSoma,OprAritSub,
+    OprRealMenor, OprRealMenorIgual,OprRealMaior,OprRelMaiorIgual,OprRelIgual,
+    OprRelDif, OpBoolE, OpBoolOu, AbrePar, FechaPar, Var, NumInt, NumReal,
+    Cadeia
+}
